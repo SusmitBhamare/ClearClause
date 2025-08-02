0e8eef6c0e046782d5049b5217695701bc2f1f80
susmitbhamare <susmitbhamare@gmail.com>
2025-08-02 16:40:13 +0530

    feat: complete backend setup with Express, MongoDB, Redis, and AI integration

---
+++ client/src/app/(private)/dashboard/contract/[id]/_components/contract-results.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import ContractAnalysisResults from '@/components/analysis/contract-analysis-results';
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { ContractAnalysis } from '@/interfaces/contract.interface';
+import { api } from '@/lib/api';
+import { notFound } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+interface IContractResultsProps {
+  contractId: string;
+}
+
+export default function ContractResults({ contractId }: IContractResultsProps) {
+  const { user } = useCurrentUser();
+  const [anaysisResults, setAnalysisResults] =
+    useState<ContractAnalysis | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (user) {
+      fetchAnalysisResults(contractId);
+    }
+  }, [user]);
+
+  const fetchAnalysisResults = async (id: string) => {
+    try {
+      setLoading(true);
+      const response = await api.get(`/contracts/contract/${id}`);
+      setAnalysisResults(response.data);
+      console.log('Analysis results:', response.data);
+      setError(false);
+    } catch (error) {
+      console.error('Error fetching analysis results:', error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center p-8'>
+        <div className='text-gray-500 font-medium'>Loading contract analysis...</div>
+      </div>
+    );
+  }
+
+  if (error || !anaysisResults) {
+    return notFound();
+  }
+
+  return (
+    <ContractAnalysisResults 
+      anaysisResults={anaysisResults} 
+      isActive={false}
+      contractId={contractId}
+    />
+  );
+}
